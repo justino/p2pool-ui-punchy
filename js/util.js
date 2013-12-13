52933ae36cba327e8bef7611d5fbedba8a722172
Alexander Zschach <alex@zschach.net>
2013-12-13 22:28:16 +0100

    Ongoing development
    Fetching and filling of status section. Minor cleanups.
    
    Ongoing development
    
    First bad hack shot

--- js/util.js
+++ js/util.js
@@ -14,3 +14,19 @@ var formatHashrate= function(rate) {
 }
 
 // ======================================================================
+// format seconds to an interval like '1d 7h 5s'
+
+String.prototype.formatSeconds = function () {
+    var sec_num = parseInt(this, 10);
+    var days    = Math.floor(sec_num / 86400);
+    var hours   = Math.floor((sec_num - (days * 86400)) / 3600);
+    var minutes = Math.floor((sec_num - (days * 86400  + hours * 3600)) / 60);
+    var seconds = sec_num - (days * 86400) - (hours * 3600) - (minutes * 60);
+
+    var time= '';
+    if(days > 0) time+= days + 'd ';
+    time += hours + 'h ' + minutes + 'm ' + seconds + ' s';
+    return time;
+}
+
+// ======================================================================
