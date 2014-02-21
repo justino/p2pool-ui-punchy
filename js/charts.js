04d259a1eb48ab42784fec1bff0e179b707a875b
Alexander Zschach <alex@zschach.net>
2014-02-21 15:22:53 +0100

    Added possibility to configure per external JSON config file

--- js/charts.js
+++ js/charts.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 var draw= function(hashrate, doarate, renderTo) {
   char= new Highcharts.Chart({
     chart: {
@@ -10,29 +8,6 @@ var draw= function(hashrate, doarate, renderTo) {
     credits: { enabled: false },
     exporting: { enabled: false },
     title: { text: 'Node Hash/DOA-Rate' },
-=======
-var draw= function(hashrate, doarate) {
-=======
-var draw= function(hashrate, doarate, renderTo) {
->>>>>>> 55d13d9... Merged from devel tree
-  char= new Highcharts.Chart({
-    chart: {
-      renderTo: renderTo,
-      animation: true,
-      borderWidth: 0,
-    },
-    credits: { enabled: false },
-    exporting: { enabled: false },
-<<<<<<< HEAD
-    title: { text: 'Hashrate/DOA rate' },
-    rangeSelector: {
-      selected: 1,
-      enabled: true,
-    },
->>>>>>> e7bc537... First shot. Only partly done yet.  Still under high development.  Consider as alpha
-=======
-    title: { text: 'Node Hash/DOA-Rate' },
->>>>>>> 55d13d9... Merged from devel tree
     xAxis: {
       type: 'datetime',
       maxZoom: 24 * 3600,
@@ -41,10 +16,6 @@ var draw= function(hashrate, doarate, renderTo) {
       }
     },
     yAxis: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 55d13d9... Merged from devel tree
       labels: {
         formatter: function() {
           if(this.value > Math.pow(1000, 4))
@@ -58,21 +29,12 @@ var draw= function(hashrate, doarate, renderTo) {
           return this.value + ' H/s';
         },
       },
-<<<<<<< HEAD
-=======
->>>>>>> e7bc537... First shot. Only partly done yet.  Still under high development.  Consider as alpha
-=======
->>>>>>> 55d13d9... Merged from devel tree
       title: {
         text: 'Hashrate'
       },
       min: 0,
     },
     tooltip: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 55d13d9... Merged from devel tree
       formatter: function() {
         var s= '<b>' + new Date(this.x) + '</b>';
 
@@ -102,12 +64,6 @@ var draw= function(hashrate, doarate, renderTo) {
       },
       shared: true,
       valueSuffix: ' H/s',
-<<<<<<< HEAD
-=======
-      shared: true
->>>>>>> e7bc537... First shot. Only partly done yet.  Still under high development.  Consider as alpha
-=======
->>>>>>> 55d13d9... Merged from devel tree
     },
     legend: {
       enabled: true,
@@ -117,8 +73,6 @@ var draw= function(hashrate, doarate, renderTo) {
       areaspline: {
         fillColor: '#ace',
         marker: { enabled: false },
-<<<<<<< HEAD
-<<<<<<< HEAD
         lineWidth: 2,
         shadow: false,
         states: {
@@ -136,47 +90,13 @@ var draw= function(hashrate, doarate, renderTo) {
         },
         threshold: null,
         animation: false,
-=======
-        lineWidth: 1,
-=======
-        lineWidth: 2,
->>>>>>> 55d13d9... Merged from devel tree
-        shadow: false,
-        states: {
-          hover: { lineWidth: 2 }
-        },
-        threshold: null,
-        animation: false
-      },
-      spline: {
-        marker: { enabled: false },
-        lineWidth: 2,
-        shadow: false,
-        states: {
-          hover: { lineWidth: 2 }
-        },
-<<<<<<< HEAD
-        threshold: null
->>>>>>> e7bc537... First shot. Only partly done yet.  Still under high development.  Consider as alpha
-=======
-        threshold: null,
-        animation: false,
->>>>>>> 55d13d9... Merged from devel tree
       },
     },
     series: [{
       type: 'areaspline',
       name: 'Hashrate',
       data: hashrate,
-<<<<<<< HEAD
-<<<<<<< HEAD
       lineWidth: 1,
-=======
-      lineWidth: 2,
->>>>>>> e7bc537... First shot. Only partly done yet.  Still under high development.  Consider as alpha
-=======
-      lineWidth: 1,
->>>>>>> 55d13d9... Merged from devel tree
       marker: {
         enabled: false
       },
@@ -192,4 +112,3 @@ var draw= function(hashrate, doarate, renderTo) {
     }]
   });
 };
-
